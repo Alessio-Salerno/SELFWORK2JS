97f2e7b82fc4ab72ac89cbfb2922069934341e2e
Alessio-Salerno <156314493+Alessio-Salerno@users.noreply.github.com>
2024-05-18 16:03:53 +0200

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,20 @@
+ alert('ciao a tutti') ;
+
+// SOMMA TOTALE TRA LE VARIABILI
+
+let a = 5;
+let b = 10;
+let c = 15;
+let d = 20;
+let e = 25;
+let somma = a + b + c + d + e;
+console.log(somma);
+
+
+
+// MEDIA TRA LE VARAIBILI (I NUMERI SONO COLLEGATI A QUELLI DI POCOFA, QUINDI HO FATTO IL TOTALE DIVISO I NUMERI E HO TROVATO LA MEDIA)
+
+let num1 = 75 ;
+let num2 = 5 ;
+let media = num1 / num2;
+console.log(media);
